912fec3ad073a96b0c2e89a0a8d464233b30502d
Davi <ifbadavi@gmail.com>
2025-06-17 14:50:15 -0300

    feat: Add Listing ressources to show and list data

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,6 @@
 <template>
-  <div><Link href="/">Main Page</Link></div>
-  <div><Link href="/hello">Show Page</Link></div>
-  <slot>Page: </slot>
+  <slot></slot>
 </template>
 
 <script setup>
-import {Link} from '@inertiajs/vue3'
 </script>
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+Route::resource('listing', \App\Http\Controllers\ListingController::class)->only(['index','show']);
 
