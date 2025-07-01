3a3de4af618202f0403794374a5b29900e85c219
Davi <ifbadavi@gmail.com>
2025-06-30 21:19:41 -0300

    refactor: Improves listing layout with responsive grid and Box component

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,14 +1,16 @@
 <template>
-  <div v-for="listing in listings" :key="listing.id">
-    <Link :href="route('listing.show', {listing: listing.id})">
-      <ListingAddress :listing="listing" />
-    </Link>
-    <div>
-      <Link :href="route('listing.edit', {listing: listing.id})">Edit</Link>
-    </div>
-    <div>
-      <Link :href="route('listing.destroy', {listing: listing.id})" method="delete">Delete</Link>
-    </div>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+    <Box v-for="listing in listings" :key="listing.id">
+      <Link :href="route('listing.show', {listing: listing.id})">
+        <ListingAddress :listing="listing" />
+      </Link>
+      <div>
+        <Link :href="route('listing.edit', {listing: listing.id})">Edit</Link>
+      </div>
+      <div>
+        <Link :href="route('listing.destroy', {listing: listing.id})" method="delete">Delete</Link>
+      </div>
+    </Box>
   </div>
 </template>
 
@@ -16,6 +18,7 @@
 import {Link} from '@inertiajs/vue3'
 import ListingAddress from '@/Components/ListingAddress.vue'
 import { route } from 'ziggy'
+import Box from '@/Components/UI/Box.vue'
 
 defineProps({
   listings: Array,
