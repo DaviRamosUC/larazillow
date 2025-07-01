9d98ab1886586bcde6e4ad96ae4f19111fc1aa78
Davi <ifbadavi@gmail.com>
2025-06-30 21:12:12 -0300

    refactor: Style main layout and adding tailwindcss framework

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -25,7 +25,7 @@ class ListingController extends Controller
      */
     public function create()
     {
-        //
+        return inertia('Listing/Create');
     }
 
     /**
@@ -33,7 +33,18 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Listing::create($request->validate([
+            'beds'=> 'required|integer|min:1',
+            'baths'=> 'required|integer|min:1',
+            'area'=> 'required|integer|min:1',
+            'city'=> 'required|string',
+            'code'=> 'required|string',
+            'street'=> 'required|string',
+            'street_nr'=> 'required|integer|min:1',
+            'price'=> 'required|numeric|min:0',
+        ]));
+
+        return redirect()->route('listing.index')->with('success', 'Listing created successfully.');
     }
 
     /**
@@ -52,24 +63,43 @@ class ListingController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Listing $listing)
     {
-        //
+        return inertia(
+            'Listing/Edit', 
+            [
+                'listing'=> $listing
+            ]
+            );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Listing $listing)
     {
-        //
+        $listing->update(
+            $request->validate([
+            'beds'=> 'required|integer|min:1',
+            'baths'=> 'required|integer|min:1',
+            'area'=> 'required|integer|min:1',
+            'city'=> 'required|string',
+            'code'=> 'required|string',
+            'street'=> 'required|string',
+            'street_nr'=> 'required|integer|min:1',
+            'price'=> 'required|numeric|min:0',
+        ]));
+
+        return redirect()->route('listing.index')->with('success', 'Listing changed successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Listing $listing)
     {
-        //
+        $listing->delete();
+        return redirect()->back()->with('success', 'Listing was deleted.');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
-Route::resource('listing', \App\Http\Controllers\ListingController::class)->only(['index','show']);
+Route::resource('listing', \App\Http\Controllers\ListingController::class);
 
