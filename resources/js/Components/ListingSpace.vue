d195d5988945118e0b30a76864b46379acd085e4
Davi <ifbadavi@gmail.com>
2025-06-30 21:50:53 -0300

    Updates code formatting and refactors listing components
    Reduces indent size in `.editorconfig` from 4 to 2 for consistency.
    
    Refactors `ListingAddress.vue` by splitting address and price onto separate lines for improved readability.
    
    Enhances `Listing/Index.vue` by introducing `Price` and `ListingSpace` components for better separation of concerns and modularity.
    
    Adds imports for new components to ensure proper functionality.

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span class="font-bold "> {{ listing.beds }} bds <span class="text-gray-400"> |  </span> </span>
+    <span class="font-bold "> {{ listing.baths }} ba <span class="text-gray-400"> |  </span> {{ listing.area }} m² </span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  listing: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -2,7 +2,9 @@
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
     <Box v-for="listing in listings" :key="listing.id">
       <Link :href="route('listing.show', {listing: listing.id})">
-        <ListingAddress :listing="listing" />
+        <Price :price="listing.price" class="text-2xl font-bold" />
+        <ListingSpace :listing="listing" class="text-lg" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
       </Link>
       <div>
         <Link :href="route('listing.edit', {listing: listing.id})">Edit</Link>
@@ -19,6 +21,8 @@ import {Link} from '@inertiajs/vue3'
 import ListingAddress from '@/Components/ListingAddress.vue'
 import { route } from 'ziggy'
 import Box from '@/Components/UI/Box.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
+import Price from '@/Components/Price.vue'
 
 defineProps({
   listings: Array,
